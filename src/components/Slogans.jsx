09f810a598f942721966c8944a9469535e8a05af
parthpandyappp <parthpandyappp@gmail.com>
2022-11-22 14:28:47 +0530

    [Feat]: Tooltip

--- src/components/Slogans.jsx
+++ src/components/Slogans.jsx
@@ -1,3 +1,5 @@
+import ReactTooltip from "react-tooltip";
+
 const Slogans = ({ slogans }) => {
   return (
     <div>
@@ -6,9 +8,19 @@ const Slogans = ({ slogans }) => {
         <button className="btn-secondary">Download all</button>
       </div>
       <div className="slogan-items">
+        <ReactTooltip place="left" type="dark" effect="solid" />
+
         {slogans.length &&
           slogans.map((slogan) => {
-            return <p className="slogan-item">{slogan.slogan}</p>;
+            return (
+              <p
+                key={slogan.id}
+                className="slogan-item"
+                data-tip="copy to clipboard"
+              >
+                {slogan.slogan}
+              </p>
+            );
           })}
       </div>
     </div>
